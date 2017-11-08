8b4e199bb6815dfc05ad8f1f29af48f2570247ce
Andrei Straut <andrei.straut@gmail.com>
2017-11-08 09:29:15 +0000

    Rename services

--- src/main/webapp/app/JenkinsDataAnalyzer/components/jenkins-basic-statistics.component.ts
+++ src/main/webapp/app/JenkinsDataAnalyzer/components/jenkins-basic-statistics.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input, SimpleChanges, OnInit } from '@angular/core';
+import {Component, Input, SimpleChanges, OnInit} from '@angular/core';
 
-import { Logger } from 'angular2-logger/core';
-import { UtilService } from '../../Util/services/util.service';
-import { IJenkinsData } from 'jenkins-api-ts-typings';
+import {Logger} from 'angular2-logger/core';
+import {UtilService} from '../../Util/services/util.service';
+import {IJenkinsData} from 'jenkins-api-ts-typings';
 
-import { JenkinsBasicJobStatistics } from '../services/JenkinsBasicJobStatistics';
-import { JenkinsBasicBuildStatistics } from '../services/JenkinsBasicBuildStatistics';
-import { JenkinsChangeSetStatistics } from '../services/JenkinsChangeSetStatistics';
-import { JenkinsNodeStatistics } from '../services/JenkinsNodeStatistics';
+import {JenkinsBasicJobStatistics} from '../services/jenkins-basic-job-statistics.service';
+import {JenkinsBasicBuildStatistics} from '../services/jenkins-basic-build-statistics.service';
+import {JenkinsChangeSetStatistics} from '../services/jenkins-change-set-statistics.service';
+import {JenkinsNodeStatistics} from '../services/jenkins-node-statistics.service';
 
-import { StatisticsCardEntry } from '../services/StatisticsCardEntry';
-    
+import {StatisticsCardEntry} from '../services/StatisticsCardEntry';
+
 @Component({
     selector: 'jenkins-basic-statistics',
     templateUrl: 'app/JenkinsDataAnalyzer/templates/jenkins-basic-statistics.template.html',
@@ -19,10 +19,10 @@ import { StatisticsCardEntry } from '../services/StatisticsCardEntry';
 export class JenkinsBasicStatisticsComponent implements OnInit {
     @Input('utilService')
     utilService: UtilService;
-    
+
     @Input('jenkinsData')
     jenkinsData: IJenkinsData;
-    
+
     ngOnChanges(changes: SimpleChanges): void {
         if (changes["utilService"] !== undefined && changes["utilService"].currentValue !== undefined) {
             this.utilService = changes["utilService"].currentValue;
@@ -30,39 +30,39 @@ export class JenkinsBasicStatisticsComponent implements OnInit {
         if (changes["jenkinsData"] !== undefined && changes["jenkinsData"].currentValue !== undefined) {
             this.jenkinsData = changes["jenkinsData"].currentValue;
         }
-        
+
         if (this.utilService !== undefined && !this.utilService.isInvalid(this.jenkinsData)) {
             this.analyze(this.jenkinsData);
         }
     }
-    
+
     public dataAvailable: boolean;
     public analyzerData: Array<StatisticsCardEntry> = new Array<StatisticsCardEntry>();
-    
+
     basicStatistics: StatisticsCardEntry;
     basicBuildStatistics: StatisticsCardEntry;
     basicCommitStatistics: StatisticsCardEntry;
     basicNodeStatistics: StatisticsCardEntry;
-    
+
     constructor(private LOGGER: Logger) {}
-    
+
     ngOnInit() {}
-    
-    analyze(jenkinsData: IJenkinsData):void {
-        
+
+    analyze(jenkinsData: IJenkinsData): void {
+
         this.analyzerData = new Array<StatisticsCardEntry>();
         this.dataAvailable = true;
-        
+
         this.basicStatistics = new JenkinsBasicJobStatistics(this.utilService, this.LOGGER, jenkinsData).getStatistics();
         this.basicBuildStatistics = new JenkinsBasicBuildStatistics(this.utilService, this.LOGGER, jenkinsData).getStatistics();
         this.basicCommitStatistics = new JenkinsChangeSetStatistics(this.utilService, this.LOGGER, jenkinsData).getStatistics();
         this.basicNodeStatistics = new JenkinsNodeStatistics(this.utilService, this.LOGGER, jenkinsData).getStatistics();
-        
+
         this.LOGGER.debug("Basic Job Statistics", this.basicStatistics);
         this.LOGGER.debug("Basic Build Statistics", this.basicBuildStatistics);
         this.LOGGER.debug("Basic Commit Statistics", this.basicCommitStatistics);
         this.LOGGER.debug("Basic Node Statistics", this.basicNodeStatistics);
-        
+
         this.analyzerData.push(this.basicStatistics);
         this.analyzerData.push(this.basicBuildStatistics);
         this.analyzerData.push(this.basicCommitStatistics);
--- src/main/webapp/app/JenkinsDataAnalyzer/components/jenkins-job-relationship-network.component.ts
+++ src/main/webapp/app/JenkinsDataAnalyzer/components/jenkins-job-relationship-network.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, SimpleChanges, OnInit } from '@angular/core';
-import { Options, Network } from 'vis';
-import { IJenkinsData } from 'jenkins-api-ts-typings';
+import {Component, Input, SimpleChanges, OnInit} from '@angular/core';
+import {Options, Network} from 'vis';
+import {IJenkinsData} from 'jenkins-api-ts-typings';
 
-import { UtilService } from '../../Util/services/util.service';
-import { VisNetworkData } from '../services/VisNetworkData';
-import { VisDataSetItem } from '../services/VisDataSetItem';
-import { VisEventNetworkProperties } from '../services/VisEventNetworkProperties';
-import { Logger } from 'angular2-logger/core';
+import {UtilService} from '../../Util/services/util.service';
+import {VisNetworkData} from '../services/VisNetworkData';
+import {VisDataSetItem} from '../services/VisDataSetItem';
+import {VisEventNetworkProperties} from '../services/VisEventNetworkProperties';
+import {Logger} from 'angular2-logger/core';
 
-import { JobRelationshipNetworkService } from '../services/JobRelationshipNetworkService';
+import {JobRelationshipNetworkService} from '../services/jenkins-job-relationship-network.service';
 
 @Component({
     selector: 'jenkins-job-relationship-network',
@@ -18,10 +18,10 @@ import { JobRelationshipNetworkService } from '../services/JobRelationshipNetwor
 export class JenkinsJobRelationshipNetworkComponent implements OnInit {
     @Input('utilService')
     utilService: UtilService;
-    
+
     @Input('jenkinsData')
     jenkinsData: IJenkinsData;
-    
+
     ngOnChanges(changes: SimpleChanges): void {
         if (changes["utilService"] !== undefined && changes["utilService"].currentValue !== undefined) {
             this.utilService = changes["utilService"].currentValue;
@@ -29,7 +29,7 @@ export class JenkinsJobRelationshipNetworkComponent implements OnInit {
         if (changes["jenkinsData"] !== undefined && changes["jenkinsData"].currentValue !== undefined) {
             this.jenkinsData = changes["jenkinsData"].currentValue;
         }
-        
+
         if (this.utilService !== undefined && !this.utilService.isInvalid(this.jenkinsData)) {
             this.analyze(this.jenkinsData);
         }
@@ -40,18 +40,18 @@ export class JenkinsJobRelationshipNetworkComponent implements OnInit {
     private visNetworkOptions: Options;
     private visJobNetworkData: VisNetworkData = new VisNetworkData();
     private visNetwork: Network;
-    
+
     private readonly verticalMultiplier = 300;
     private readonly verticalSubMultiplier = 50;
     private readonly horizontalMultiplier = 350;
     private readonly maxHorizontalItemsPerLevel = 20;
-    
+
     private tooltipDelay = 1;
     private timeLimitUnit = "seconds";
-    
+
     private showNodesWithEdges: boolean = true;
     private showNodesWithoutEdges: boolean = true;
-    
+
     constructor(private LOGGER: Logger) {}
 
     ngOnInit() {
@@ -62,66 +62,66 @@ export class JenkinsJobRelationshipNetworkComponent implements OnInit {
     analyze(data: IJenkinsData): Network {
         let jobService: JobRelationshipNetworkService = new JobRelationshipNetworkService(
             this.utilService,
-            data, 
-            this.verticalMultiplier, 
-            this.verticalSubMultiplier, 
-            this.horizontalMultiplier, 
+            data,
+            this.verticalMultiplier,
+            this.verticalSubMultiplier,
+            this.horizontalMultiplier,
             this.maxHorizontalItemsPerLevel);
         let parent = this;
-        
+
         this.visJobNetworkData = jobService.getDataSet(this.showNodesWithoutEdges, this.showNodesWithEdges);
         this.visNetwork = new Network(this.visNetworkContainer, this.visJobNetworkData, this.visNetworkOptions);
         this.visNetwork.fit();
-        
-        this.visNetwork.on('doubleClick', function(properties:VisEventNetworkProperties) {
+
+        this.visNetwork.on('doubleClick', function (properties: VisEventNetworkProperties) {
             if (parent.utilService.isInvalid(parent.visJobNetworkData) || parent.utilService.isInvalid(parent.visJobNetworkData.nodes)) {
                 return;
             }
-            
+
             if (parent.utilService.isInvalid(properties) || parent.utilService.isInvalid(properties.nodes)) {
                 return;
             }
-            
-            let item:VisDataSetItem = parent.visJobNetworkData.nodes.get(properties.nodes[0]);
+
+            let item: VisDataSetItem = parent.visJobNetworkData.nodes.get(properties.nodes[0]);
             if (parent.utilService.isInvalid(item) || parent.utilService.isInvalid(item.url)) {
                 return;
             }
-            
+
             window.open(item.url, '_blank');
         });
 
         this.LOGGER.debug("Job Build Data", this.visJobNetworkData);
-        
+
         return this.visNetwork;
     }
-    
+
     toggleUnconnectedNodes() {
         this.showNodesWithoutEdges = !this.showNodesWithoutEdges;
-        
+
         let jobService: JobRelationshipNetworkService = new JobRelationshipNetworkService(
             this.utilService,
-            this.jenkinsData, 
-            this.verticalMultiplier, 
-            this.verticalSubMultiplier, 
-            this.horizontalMultiplier, 
+            this.jenkinsData,
+            this.verticalMultiplier,
+            this.verticalSubMultiplier,
+            this.horizontalMultiplier,
             this.maxHorizontalItemsPerLevel);
-        
+
         let jobsData = jobService.getDataSet(this.showNodesWithoutEdges, this.showNodesWithEdges);
         this.visNetwork.setData(jobsData);
         this.visNetwork.fit();
     }
-    
+
     toggleConnectedNodes() {
         this.showNodesWithEdges = !this.showNodesWithEdges;
-        
+
         let jobService: JobRelationshipNetworkService = new JobRelationshipNetworkService(
             this.utilService,
-            this.jenkinsData, 
-            this.verticalMultiplier, 
-            this.verticalSubMultiplier, 
-            this.horizontalMultiplier, 
+            this.jenkinsData,
+            this.verticalMultiplier,
+            this.verticalSubMultiplier,
+            this.horizontalMultiplier,
             this.maxHorizontalItemsPerLevel);
-        
+
         let jobsData = jobService.getDataSet(this.showNodesWithoutEdges, this.showNodesWithEdges);
         this.visNetwork.setData(jobsData);
         this.visNetwork.fit();
@@ -146,10 +146,10 @@ export class JenkinsJobRelationshipNetworkComponent implements OnInit {
                 },
                 font: {
                     size: 25,
-                },      	
+                },
                 fixed: {
                     y: true
-    		}
+                }
             },
             edges: {
                 smooth: false,
