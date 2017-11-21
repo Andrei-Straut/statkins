e83b48974d679273528a8e3b3dfa6ef2a8cf8d5e
Andrei Straut <andrei.straut@gmail.com>
2017-11-21 18:10:24 +0000

    Add HTML test and code coverage report

--- src/main/webapp/karma.conf.js
+++ src/main/webapp/karma.conf.js
@@ -8,25 +8,16 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-coverage-istanbul-reporter'),
             require('karma-junit-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('karma-htmlfile-reporter'),
             require('@angular/cli/plugins/karma')
         ],
-        client: {
-            clearContext: false // leave Jasmine Spec Runner output visible in browser
-        },
-        coverageIstanbulReporter: {
-            reports: ['html', 'lcovonly'],
-            fixWebpackSourcePaths: true
-        },
-        angularCli: {
-            environment: 'dev'
-        },
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
+        singleRun: true,
         browsers: ['ChromeHeadless'],
         customLaunchers: {
             ChromeHeadless: {
@@ -39,17 +30,36 @@ module.exports = function (config) {
                 ]
             }
         },
-        singleRun: true,
-        reporters: ['progress', 'junit'],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        angularCli: {
+            environment: 'dev'
+        },
+        reporters: ['progress', 'junit', 'html', 'coverage-istanbul'],
+        /** HTML Test Report */
+        htmlReporter: {
+            outputFile: 'dist/test-results/test-report.html',
+            groupSuites: true,
+            useCompactStyle: true,
+            useLegacyStyle: true
+        },
+        /** jUnit-style XML Test Report */
         junitReporter: {
-            outputDir: '', // results will be saved as $outputDir/$browserName.xml
-            outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+            outputDir: 'dist/test-results', // results will be saved as $outputDir/$browserName.xml
+            outputFile: 'test-report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
             suite: '', // suite will become the package name attribute in xml testsuite element
             useBrowserName: true, // add browser name to report and classes names
             nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
             classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
             properties: {}, // key value pair of properties to add to the <properties> section of the report
             xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+        },
+        /** HTML Coverage Report */
+        coverageIstanbulReporter: {
+            dir: 'dist/test-results/coverage',
+            reports: ['html', 'json'],
+            fixWebpackSourcePaths: true
         }
     });
 };
