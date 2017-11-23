5902d31198a6a65240deee90295c2bad00152174
Andrei Straut <andrei.straut@gmail.com>
2017-11-23 12:35:41 +0000

    Add remaining tests for app component

--- src/main/webapp/src/app/app.component.spec.ts
+++ src/main/webapp/src/app/app.component.spec.ts
@@ -3,9 +3,12 @@ import {AppComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {IJenkinsData} from 'jenkins-api-ts-typings';
 
 import {JenkinsDataRetrieverModule} from './jenkins-data-retriever/jenkins-data-retriever.module';
 import {JenkinsDataAnalyzerModule} from './jenkins-data-analyzer/jenkins-data-analyzer.module';
+
+import {IJenkinsDataMockService} from './test-mock/services/jenkins-data.mock.service';
 
 describe('AppComponent', () => {
     
@@ -22,13 +25,13 @@ describe('AppComponent', () => {
     
     it('should create the app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
+        const app:AppComponent = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
     
     it(`should have as title 'Statkins'`, async(() => {
         const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
+        const app:AppComponent = fixture.debugElement.componentInstance;
         expect(app.name).toEqual('Statkins');
     }));
     
@@ -37,5 +40,20 @@ describe('AppComponent', () => {
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h1').textContent).toContain('Statkins');
+    }));
+    
+    it('jenkinsData should be undefined when component is started', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app:AppComponent = fixture.debugElement.componentInstance;
+        expect(app.jenkinsData).toBeUndefined();
+    }));
+    
+    it('jenkinsData should have correct value when onDataRetrieved is triggered', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app:AppComponent = fixture.debugElement.componentInstance;
+        const data: IJenkinsData = new IJenkinsDataMockService();
+        app.onDataRetrieved(data);
+        expect(app.jenkinsData).toBeDefined();
+        expect(app.jenkinsData).toBe(data);
     }));
 });
