7ec57860731881131f0f7180f47884cd1490fcef
Andrei Straut <andrei.straut@gmail.com>
2017-12-12 14:51:43 +0000

    ProxyService tests

--- src/main/webapp/src/app/proxy/services/proxy.service.spec.ts
+++ src/main/webapp/src/app/proxy/services/proxy.service.spec.ts
@@ -1,20 +1,48 @@
 import {TestBed, inject} from '@angular/core/testing';
-import {HttpModule} from '@angular/http';
+import {HttpModule, XHRBackend, Response, ResponseOptions} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
 import {CommonModule} from '@angular/common';
 
 import {ProxyService} from './proxy.service';
 import {ConfigService} from '../../config/services/config.service';
 import {Logger} from '../../../../node_modules/angular2-logger/core';
 
+const mockResponse = {
+    data: [
+        {id: 0, data: 'SomeData'}
+    ]
+};
+
 describe('ProxyService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [CommonModule, HttpModule],
-            providers: [ProxyService, Logger, ConfigService]
+            providers: [ProxyService, Logger, ConfigService, {provide: XHRBackend, useClass: MockBackend}]
         });
     });
 
-    it('should be created', inject([ProxyService], (service: ProxyService) => {
-        expect(service).toBeTruthy();
+    it('proxy should return correct data', inject([ProxyService, XHRBackend], (service: ProxyService, mockHttp: MockBackend) => {
+        mockHttp.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(mockResponse)
+            })));
+        });
+
+        service.proxy("http://www.SomeUrl123456SomeUrl.com").subscribe(response => {
+            expect(response).toBeTruthy();
+            expect(((response as JSON)["data"])[0]).toEqual({id: 0, data: 'SomeData'});
+        });
+    }));
+
+    it('proxyRaw should return correct data', inject([ProxyService, XHRBackend], (service: ProxyService, mockHttp: MockBackend) => {
+        mockHttp.connections.subscribe((connection: any) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(mockResponse)
+            })));
+        });
+
+        service.proxyRaw("http://www.SomeUrl123456SomeUrl.com").subscribe(response => {
+            expect(response).toBeTruthy();
+        });
     }));
 });
